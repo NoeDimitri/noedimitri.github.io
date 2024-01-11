975ff3a31e4ca548753826fcc2b0b6dfca37d70c
NoeDimitri <justind4@usf.edu>
2024-01-10 19:52:45 -0500

    i have no idea what is going on

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          whyy did that workk
+          please work lol
+        </p>
       </header>
     </div>
   );
