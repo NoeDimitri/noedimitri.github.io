c9eae3b15e1fe16b9707546ba1d4244a65aaa6b5
NoeDimitri <71399587+NoeDimitri@users.noreply.github.com>
2024-01-12 14:07:23 -0500

    starting to toy with art

--- src/ArtsDesigns.jsx
+++ src/ArtsDesigns.jsx
@@ -1,29 +1,40 @@
 import './App.css'
 import React from "react";
+import Image from 'react-bootstrap/Image';
 
 // import goofy_art from './images/k.png'
+
+const designs = [
+    {
+        id: 0,
+        src: "/images/gamejam.png"},
+    {
+        id: 1,
+        src: "/images/research_roadmap.png"},
+    {
+        id: 2,
+        src: "/images/showcase.png"},    
+    {
+        id: 3,
+        src: "/images/discord_bot.png"},
+]
+
+const designItems = designs.map(design => 
+    <div class="art_item"><Image src={design.src} rounded /> </div>
+)   
+
+
 
 class ArtDesigns extends React.Component{
     render() {
         return(
             <div className='Tab-Content'>
-                Art and Designs I've made <br/>
+                Art <br/>
+                    <ArtItem link="images/fran.png"></ArtItem>
+                    <ArtItem link="/logo512.png"></ArtItem>
+                Designs
                 <div class="art_grid">
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
-                    <ArtItem></ArtItem>
+                    {designItems}
                 </div>
             </div>
         );
@@ -33,9 +44,7 @@ class ArtDesigns extends React.Component{
 function ArtItem({link})
 {
     return(
-        <div class="art_item">
-            <img src="/fran.png" alt="pretty art!!"></img>
-        </div>
+        <Image class="art-item" src={link} rounded />
     );
 }
 
